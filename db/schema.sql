61a17ca13099dd9848e74054f07378f3425f34a8
Youssef <youssefkaroui6@gmail.comm>
2023-08-04 13:40:39 -0400

    schema.sql file done; added all tables needed

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,36 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db
-USE employee_db 
+USE employee_db;
+
+
+CREATE TABLE department (
+    id INT  AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE  role (
+    id INT  AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department (id)
+    ON DELETE SET NULL
+
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+
+);
