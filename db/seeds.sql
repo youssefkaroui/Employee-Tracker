6d7a11c77b1c752cdbc978252e5acfb9ce0ab287
Youssef <youssefkaroui6@gmail.comm>
2023-08-04 14:54:04 -0400

    seeds.sql ccomplete! long list

--- db/seeds.sql
+++ db/seeds.sql
@@ -26,6 +26,18 @@ VALUES
 ('Robert', 'Funny', 2, 1),
 ('Ben', 'Active', 3, 1),
 ('Keysi', 'Happy', 4, 1),
+('Laurent', 'Frenchy', 5, 1),
+('Diego', 'Angry', 6, 5),
+('Marck', 'Cold', 7, 5),
+('Panos', 'Fast', 8, 5),
+('Topher', 'Calm', 9, 3),
+('Dave', 'late', 10, 3),
+('Dina', 'Efficeint', 10, 3),
+('Phil', 'Jumpy', 11, 3),
+('Katie', 'Smiles', 12, 3),
+('Micky', 13, 3);
+
+
 
 
 
